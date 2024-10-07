eb746a7d488d96aa11fa67cc3f880fbde68a1fed
Artur <grigorevich1993artur@gmail.com>
2024-10-07 13:17:08 +0400

    main

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',   // output directory
+			assets: 'build',  // output directory
+			fallback: null
+		})
 	},
 	preprocess: vitePreprocess(),
 
